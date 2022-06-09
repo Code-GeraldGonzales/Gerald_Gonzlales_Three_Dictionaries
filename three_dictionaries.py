22d33f092499c33584dc2cfc81eee54eb8a3041f
Gerald Gonzales <jerrygonzales417@gmail.com>
2022-06-08 18:25:24 -0700

    initial_commit

---
+++ three_dictionaries.py
@@ -0,0 +1,4 @@
+mylist = [{"name":'Charlie', "age":30, 'email': 'charlie@gmail.com'}, {"name":'Stephen', "age":31, 'email': 'stephen@gmail.com'} , {"name":'Alex', "age":27, 'email': 'alex@gmail.com'}]
+
+for i in mylist:
+    print(f"Our student's names: {i['name']}, Our ages: {i['age']}, Our emails: {i['email']}")
